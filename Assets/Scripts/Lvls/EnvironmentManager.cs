cbe88458c2eda0698d63689e5d0685c89735a728
MrSpanker <mrspankerman@gmail.com>
2023-09-10 19:11:33 +0300

    d

--- Assets/Scripts/Lvls/EnvironmentManager.cs
+++ Assets/Scripts/Lvls/EnvironmentManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class EnvironmentManager : MonoBehaviour
@@ -9,6 +10,10 @@ public class EnvironmentManager : MonoBehaviour
 
     private GameManager _gameManager;
     private GameStateManager _gameStateManager;
+
+    public string folderPath; 
+    public GameObject[] loadedObjects;
+    public int LvlNumber = 1;
 
     public void Init(GameManager gameManager, GameStateManager gameStateManager)
     {
@@ -22,5 +27,34 @@ public class EnvironmentManager : MonoBehaviour
     public void RegisterEnvironment(GameObject objectToSpawn)
     {
         _environmentList.Add(objectToSpawn);
-    }    
+    }
+    public void LoadObjects()
+    {
+        
+        string folderPath = "Assets/Lvls/Lvl " + LvlNumber;
+        
+        if (Directory.Exists(folderPath))
+        {
+            string[] files = Directory.GetFiles(folderPath);
+            loadedObjects = new GameObject[files.Length];
+
+            for (int i = 0; i < files.Length; i++)
+            {
+                GameObject obj = Resources.Load<GameObject>(folderPath + "/" + Path.GetFileNameWithoutExtension(files[i]));
+                if (obj != null)
+                {
+                    loadedObjects[i] = obj;
+                }
+                else
+                {
+                    Debug.LogWarning("Не удалось загрузить объект из файла: " + files[i]);
+                }
+            }
+        }
+        else
+        {
+            Debug.LogError("Папка не найдена: " + folderPath);
+        }
+    }
 }
+
